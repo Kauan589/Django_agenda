2046f23d1f9d8b875fdfc98f0fccc5b8d98c904b
Kauan Maximiano <kauaneduardofm@gmail.com>
2025-05-12 11:52:52 -0300

    Created the app Contacts and configured the templates ands statics path

--- contacts/urls.py
+++ contacts/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from contacts import views
+
+app_name = 'contacts'
 
 urlpatterns = [
-    path('', include('contacts.urls')), #direciona a raiz do site para a url do app contacts
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
